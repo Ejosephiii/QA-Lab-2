2f769093982867b9a1e3bc9162db79b43e24d20b
Ethan Winegardner <ejosephhiii@gmail.com>
2022-07-19 14:50:57 -0600

    Doneskiesss

---
+++ __tests__/movie-list.test.js
@@ -0,0 +1,27 @@
+const {Builder, Capabilities, By} = require('selenium-webdriver')
+
+require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeAll(async () => {
+    await driver.get('http://127.0.0.1:5500/movie-list/index.html')
+})
+
+afterAll(async () => {
+    await driver.quit()
+})
+
+test('add a movie to the list', async () =>{
+    const inputField = await driver.findElement(By.xpath('//input'))
+    await inputField.sendKeys('Tron: Legacy')
+    await driver.sleep(2000)
+
+    const theButton = await driver.findElement(By.css('button'))
+    await theButton.click()
+    await driver.sleep
+})
+
+// test('verify a movie on the list', () =>{
+    
+// })
